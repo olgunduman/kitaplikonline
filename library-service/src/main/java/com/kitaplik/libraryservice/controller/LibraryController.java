b8a594da443aeba9b41e74fe8b3406d2a4c50aa0
OlgunDuman <olgunduman49@gmail.com>
2022-12-24 14:42:52 +0300

    config server

--- library-service/src/main/java/com/kitaplik/libraryservice/controller/LibraryController.java
+++ library-service/src/main/java/com/kitaplik/libraryservice/controller/LibraryController.java
@@ -5,6 +5,8 @@ import com.kitaplik.libraryservice.dto.LibraryDto;
 import com.kitaplik.libraryservice.service.LibraryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RefreshScope
 @RequestMapping("/v1/library")
 public class LibraryController {
 
@@ -23,6 +26,9 @@ public class LibraryController {
         this.libraryService = libraryService;
         this.environment = environment;
     }
+
+    @Value("${library.service.count}")
+    private Integer count;
 
     @GetMapping("{id}")
     public ResponseEntity<LibraryDto> getLibraryById(@PathVariable  String id){
@@ -45,5 +51,10 @@ public class LibraryController {
     @GetMapping
     public ResponseEntity<List<String>> getAllLibrary(){
         return ResponseEntity.ok(libraryService.getAllLibrary());
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount(){
+        return ResponseEntity.ok("Library service count : " + count);
     }
 }
